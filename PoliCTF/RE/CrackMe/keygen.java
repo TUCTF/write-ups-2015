79d6c6ce126259970acef37f0c8a02a3a7f5314e
Ryan McCarthy <ryanaiden93@gmail.com>
2015-07-15 16:16:08 -0500

    Create keygen.java

---
+++ PoliCTF/RE/CrackMe/keygen.java
@@ -0,0 +1,65 @@
+class keygen {
+
+    public static void main(String[] args) {
+        
+        String s = "ee[[c%l][c{g}[%{%Mc%spdgj=]T%aat%=O%bRu%sc]c%ti[o%n=Wcs%=No[t=T][hct%=buga[d=As%=W]e=T%ho[u%[%g]h%t[%}%";
+        System.out.println(cDota(bDota(bDotb(bDotc(bDotd(bDotg(bDoth(bDote(bDotf(bDoti(cDotc(cDotb(cDotd(s))))))))))))));
+        
+    }
+    
+    public static String cDotd(String s)
+    {
+        return s.replace("spdgj", "yb%e");
+    }
+    public static String cDotb(String s)
+    {
+        return s.replace("aat", "his");
+    }
+    public static String cDotc(String s)
+    {
+        return s.replace("buga", "Goo");
+    }
+    public static String cDota(String s)
+    {
+        return s.replace("aa", "ca");
+    }
+
+
+
+    public static String bDoti(String s)
+    {
+        return s.replace("=", "_");
+    }
+    public static String bDotf(String s)
+    {
+        return s.replaceFirst("\\}", "");
+    }
+    public static String bDote(String s)
+    {
+        return s.replaceFirst("\\{", "");
+    }
+    public static String bDoth(String s)
+    {
+        return s.replaceFirst("R", "f");
+    }
+    public static String bDotg(String s)
+    {
+        return s.replaceFirst("c", "f");
+    }
+    public static String bDotd(String s)
+    {
+        return s.replace("]", "");
+    }
+    public static String bDotc(String s)
+    {
+        return s.replace("[", "");
+    }
+    public static String bDotb(String s)
+    {
+        return s.replace("%", "");
+    }
+    public static String bDota(String s)
+    {
+        return s.replace("c", "a");
+    }
+}
